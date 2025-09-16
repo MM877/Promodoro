cd2dbe314edc15790bf2e879d9762878a55308bd
Mohamed Ayman "memo <mfh3873@gmail.com>
2025-09-16 19:35:18 +0300

    1

---
+++ main.py
@@ -0,0 +1,94 @@
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps=0
+timer=None
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_fun():
+    global reps, timer
+    if timer:
+        wind.after_cancel(timer)
+    canvas.itemconfig(timer_text,text="00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    check_mark.config(text="")
+    reps = 0
+    timer = None
+
+
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_fun():
+    global reps
+    reps +=1
+    work_sec=WORK_MIN*60
+    short_break_sec=SHORT_BREAK_MIN*60
+    long_break_sec=LONG_BREAK_MIN*60
+    
+    #if the reps is in the first 8 -> it will return the short break
+    if reps%8==0:
+        count_down(long_break_sec)
+        title_label.config(text="Break",fg=RED)
+        
+    elif reps %2==0:
+        count_down(short_break_sec)
+        title_label.config(text="Break",fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work",fg=GREEN)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    seconds = f"{seconds:02}"   
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+
+    if count > 0:
+        global timer
+        timer = wind.after(1000, count_down, count - 1)
+    else:
+        start_fun()
+        marks = "✔" * math.floor(reps / 2)
+        check_mark.config(text=marks)
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+from tkinter import*
+
+wind=Tk()
+wind.config(padx=100,pady=50, bg =YELLOW)
+wind.title("Pomodoro")
+
+
+title_label=Label(text="Timer",fg = GREEN, font=(FONT_NAME,50, ),bg=YELLOW)
+title_label.grid(column=1,row=0)
+
+
+tomato_img=PhotoImage(file="tomato.png")
+canvas=Canvas(width=200,height=224,bg=YELLOW, highlightthickness=0)
+canvas.create_image(100,112,image=tomato_img)
+timer_text=canvas.create_text(100,130,text="00:00",fill="white", font=(FONT_NAME,35,"bold"))
+canvas.grid(column=1,row=1)
+
+
+start=Button(text="Start",highlightthickness=0,command=start_fun)
+start.grid(column=0,row=2)
+reset=Button(text="Reset",highlightthickness=0,command=reset_fun)
+reset.grid(column=2,row=2)
+
+check_mark=Label(text="✔",fg=GREEN,bg=YELLOW,highlightthickness=0, font=("", 20))
+check_mark.grid(column=1,row=3,)
+wind.mainloop()
